82b2761ae3d010173e9fc3cf3b1d9ad2fa2959ed
Irving <107070369+irving-rp@users.noreply.github.com>
2023-03-14 13:16:17 -0600

    Add files via upload

---
+++ proyectone/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.6.4.min.js+"></script>
+    <title>Practica 1 | Includes con PHP</title>
+</head>
+<body>
+
+<!-- navbar  -->
+
+<?php include "include/navbar.php";  ?>
+
+<!-- terminanavbar  -->
+
+<!-- offcanvas -->
+
+<?php include "include/menu.php";  ?>
+
+<!-- terminaoffcanvas -->
+   <script src="js/bootstrap.min.js"></script> 
+</body>
+</html>
